bc9e9f7bbbc07fec681e58218074f7ae5d93901e
Dan Mitchell <dan@digitalreflow.co.uk>
2017-06-06 11:05:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Design sprint prototype
+
+## Install
+
+```
+git clone https://github.com/alphagov/pde-design-sprint1 && cd pde-design-sprint1
+npm install
+npm start
+```
+
+Go to [http://127.0.0.1:3000](http://127.0.0.1:3000) in your browser
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (
+  window.sessionStorage && window.sessionStorage.getItem('prototypeWarning') !== 'false' &&
+  window.console && window.console.info
+) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.sessionStorage.setItem('prototypeWarning', true)
+}
+
+$(document).ready(function () {
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init()
+
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+})
+
+
+function do_switch(previewer, appender, tab){
+  $(previewer).empty().append($(appender).html());
+  $('.tab').each(function(i, elem){
+    $(elem).addClass('inactive');
+    $(elem).removeClass('active');
+  });
+  $(tab).parent().removeClass('inactive');
+  $(tab).parent().addClass('active');
+  $(document).click();
+  return false;
+}
