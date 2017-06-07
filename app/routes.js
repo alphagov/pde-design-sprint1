566a123b7d4d30a3a1247c789e6a4c0182970476
Simon Worthington <simon@simonwo.net>
2017-06-07 15:07:36 +0100

    Updates from second day of sprinting.

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,14 @@ router.get('/', function (req, res) {
 })
 
 // add your routes here
+router.get('/team-a/confirm', function (req, res) {
+  res.render('team-a/confirm.html', {
+    'data_what': "I want to receive only a yes or no answer to the Blue Badge eligibility criteria and an expiry date for the eligibility.",
+    'data_provided': "First name, surname, date of birth, National Insurance number.",
+    'data_use': "Only use the check to confirm whether a citizen automatically qualifies for a Blue Badge.",
+    'data_how': "Via the PDE API.",
+    'data_when': "Eligibility checking will happen only when citizen's Blue Badge application has been received and they have given permission to perform an eligibility check"
+  })
+})
 
 module.exports = router
